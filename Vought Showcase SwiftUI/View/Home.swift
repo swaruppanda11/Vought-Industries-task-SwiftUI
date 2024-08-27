73ef22c216d251907ad96d4eb653f9ae6f5d7c72
swaruppanda11 <swaruppanda11@gmail.com>
2024-08-28 02:05:47 +0530

    first commit

---
+++ Vought Showcase SwiftUI/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Vought Showcase SwiftUI
+//
+//  Created by Swarup Panda on 27/08/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
