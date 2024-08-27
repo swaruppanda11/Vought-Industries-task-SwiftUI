242ac1184bf502885fbf35fb0f6f286c8d4e3e2e
swaruppanda11 <swaruppanda11@gmail.com>
2024-08-27 08:06:06 +0530

    Initial Commit

---
+++ Vought Showcase SwiftUI/Vought_Showcase_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vought_Showcase_SwiftUIApp.swift
+//  Vought Showcase SwiftUI
+//
+//  Created by Swarup Panda on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Vought_Showcase_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
